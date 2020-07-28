2332b18a3e53c0b50420f9c859977d1772f71e12
guillemforto <50271709+guillemforto@users.noreply.github.com>
2020-07-28 16:00:07 +0200

    - “\n” at the end of every stop and every warning - Condition checking that population size equals the sum of the inverse  of the first order inclusion probabilities has been fixed.  There was an issue with the comparison of an object of class int  and numerical which returned False when the the two numbers were equal. - Same condition as above is now only checked when method=“si”. - We now only add an automatic pkey in case remerge=F : HTcondbiasest and strata_HTcondbiasest should work well in every case, and when more than a single interest variable is selected. Plus everything is renamed correctly.

--- functions_code.R
+++ functions_code.R
@@ -7,8 +7,9 @@
 #' @param gn  Population size
 #' @param method  Sampling design : si for simple random sampling, poisson, rejective
 #' @param pii  First order inclusion probabilities
+#' @param di  Inverse of the first order inclusion probabilities
 #' @param pkey  Primary key to keep as an identifier of every unit
-#' @param remerge  True/False to remerge the conditional bias with the original data set
+#' @param remerge  True/False to remerge the resulting conditional biases with the original data set
 #'
 #' @return Returns a data set with the conditional bias of each sampled unit
 #' @export
@@ -18,37 +19,50 @@
                              varname = NULL,
                              gn,
                              method = c("si", "poisson", "rejective"),
-                             pii,
+                             pii = NULL,
+                             di = NULL,
                              pkey = NULL,
-                             remerge=T) {
-  if (gn != sum(1/pii)) {
-    warning("Warning: the sum of the probabilities is not equal to N")
+                             remerge = T) {
+  if (nrow(data) <= 1) {
+    stop("your sample must contain at least 2 units\n")
   }
   if (missing(method)) {
-    warning("Warning: the method is not specified; by default, the method is si")
+    warning("Warning: the method is not specified; by default, the method is si\n")
     method = "si"
   }
-  if (!(method %in% c("si", "poisson", "rejective")))
-    stop("the name of the method is wrong")
-  if (method %in% c("poisson", "rejective") & missing(pii))
-    stop("the vector of probabilities is missing")
-  if (missing(gn))
-    stop("the population size is missing")
-  if (missing(pkey)) {
-    warning("Warning: no column has been specified as a primary key, one was added automatically (id)")
-    id <- c(1:nrow(data))
-  } else {
-    if (!(pkey %in% colnames(data))) {
-      stop("the specified primary key is not a column name")
-    }
-    id <- data[[pkey]]
+  if (!(method %in% c("si", "poisson", "rejective"))) {
+    stop("the name of the method is wrong\n")
   }
+  if (missing(pii) & missing(di)) {
+    stop("the vector of probabilities is missing\n")
+  } else if (missing(pii) & !missing(di)) {
+    pii <- 1 / di
+  }
+  if (missing(gn)) {
+    stop("the population size is missing\n")
+  }
+  if (method == "si" & !all.equal(gn, sum(1/pii))) {
+    warning("Warning: the sum of the probabilities is not equal to N\n")
+  }
+  if (remerge==F) {
+    if (missing(pkey)) {
+      warning("Warning: no column is specified as a primary key, one is added automatically (id)\n")
+      id <- c(1:nrow(data))
+      pkey <- "id"
+    } else {
+      if (!(pkey %in% colnames(data))) {
+        stop("the specified primary key is not a column name\n")
+      }
+      id <- data[[pkey]]
+    }
+  }
+
   data = data.frame(data)
   pn = nrow(data)
   index = 1:nrow(data)
   m = match(varname, colnames(data))
   if (any(is.na(m)))
-    stop("the name of the variable is wrong")
+    stop("the name of the variable is wrong\n")
   data2 = cbind.data.frame(data[, m], index)
   colnames(data2) = c(varname, "index")
   if (method == "si") {
@@ -66,7 +80,7 @@
     gb = t(1/pii-1)%*%as.matrix(data[,m])/gd
     bc = (1/pii -1) * (data[,m]-t(t(gb)%*%c(pii)))
   }
-  if (remerge) {
+  if (remerge == T) {
     result = cbind.data.frame(data, bc)
     colnames(result) = c(colnames(data), paste0("condbias", colnames(bc)))
   } else {
@@ -76,9 +90,7 @@
     } else {
       colnames(result) = c(paste0("condbias", colnames(bc)))
     }
-  }
-  result <- cbind.data.frame(id, result)
-  if (!(missing(pkey))) {
+    result <- cbind.data.frame(id, result)
     colnames(result)[1] <- pkey
   }
   result
@@ -95,25 +107,58 @@
 #' @param gnh  Lopulation size in each stratum
 #' @param method  Sampling design : si for simple random sampling, poisson, rejective
 #' @param pii  First order inclusion probabilities
-#' @param remerge  True/False to remerge the conditional bias with the original data set
+#' @param di  Inverse of the first order inclusion probabilities
+#' @param pkey  Primary key to keep as an identifier of every unit
+#' @param remerge  True/False to remerge the resulting conditional biases with the original data set
 #'
 #' @return Returns a dataframe with the conditional bias of each sampled unit
 #' @export
 
-"strata_HTcondbiasest" <- function(data, strataname=NULL, varname=NULL, gnh,
-                                   method=c("si","poisson","rejective"), pii, remerge=T) {
-  if (missing(gnh))
-    stop("the population size vector is missing")
-  if (missing(strataname) | is.null(strataname))
-    stop("the strata name is missing")
+
+"strata_HTcondbiasest" <- function(data,
+                                   strataname = NULL,
+                                   varname = NULL,
+                                   gnh,
+                                   method = c("si","poisson","rejective"),
+                                   pii = NULL,
+                                   di = NULL,
+                                   pkey = NULL,
+                                   remerge = T) {
+  if (missing(pii) & missing(di)) {
+    stop("the vector of probabilities is missing\n")
+  } else if (missing(pii) & !missing(di)) {
+    pii <- 1 / di
+  }
+  if (missing(gnh)) {
+    stop("the population size vector is missing\n")
+  }
+  if (missing(strataname) | is.null(strataname)) {
+    stop("no variable name to use for stratification has been specified\n")
+  }
+  if (any(table(data[,strataname]) <= 1)) {
+    stop("each stratum must contain at least 2 units\n")
+  }
+  if (remerge==F) {
+    if (missing(pkey)) {
+      warning("Warning: no column is specified as a primary key, one is added automatically (id)\n")
+      id <- c(1:nrow(data))
+      pkey <- "id"
+    } else {
+      if (!(pkey %in% colnames(data))) {
+        stop("the specified primary key is not a column name\n")
+      }
+      id <- data[[pkey]]
+    }
+  }
+
   data = data.frame(data)
   index = 1:nrow(data)
   m = match(varname, colnames(data))
   if (any(is.na(m)))
-    stop("the name of the variable is wrong")
+    stop("the name of the variable is wrong\n")
   ms = match(strataname, colnames(data))
   if (any(is.na(ms)))
-    stop("the name of the strata is wrong")
+    stop("the name of the strata is wrong\n")
   x1 = data.frame(unique(data[,ms]))
   bc = matrix(0, nrow=nrow(data), ncol=length(m))
   cgn = 0
@@ -121,24 +166,30 @@
   for (i in 1:nrow(x1)) {
     datastr = as.data.frame(data[(data[,ms]==i),m])
     colnames(datastr) = colnames(data)[m]
+    datastr[pkey] <- c(1:nrow(datastr))
     nh = nrow(as.data.frame(datastr))
     piisrt = pii[(data[,ms]==i)]
-    sum(1/piisrt) == gnh[i]
-    resint = HTcondbiasest(data=datastr, varname=varname, gn=gnh[i], method=method , pii=piisrt, remerge=F)
+    resint = HTcondbiasest(data=datastr, varname=varname, gn=gnh[i], method=method, pii=piisrt, pkey=pkey, remerge=F)
+    resint[,pkey] <- NULL
     bc[(cgn+1):(cgn+nh),] = as.matrix(resint)
     # bc[(cgn+1):(cgn+nh)]=nh[i]/(nh[i]-1)*(gnh[i]/nh-1)*(y-mean(y))
     cgn = cgn+nh
   }
-  if (remerge==T){
+  if (remerge==T) {
     result = cbind.data.frame(data, bc)
     colnames(result) = c(colnames(data), colnames(resint))
   } else {
-    result = bc
-    colnames(result) = paste0("condbias", colnames(bc))
+    result = data.frame(bc)
+    if (length(m) == 1) {
+      colnames(result) = c("condbias")
+    } else {
+      colnames(result) = c(paste0("condbias", varname))
+    }
+    result <- cbind.data.frame(id, result)
+    colnames(result)[1] <- pkey
   }
   result
 }
-
 
 
 
@@ -157,32 +208,32 @@
 "robustest" <- function (data, varname=NULL, gn,
                          method=c("si","poisson","rejective"), pii) {
   if (gn != sum(1/pii)) {
-    warning("the sum of the inclusion probabilities is not equal to N")
+    warning("the sum of the inclusion probabilities is not equal to N\n")
   }
   if (missing(method)) {
-    warning("the method is not specified; by default, the method is si")
+    warning("the method is not specified; by default, the method is si\n")
     method = "si"
   }
   if (!(method %in% c("si", "poisson", "rejective")))
-    stop("the name of the method is wrong")
+    stop("the name of the method is wrong\n")
   if (method %in% c("poisson", "rejective") & missing(pii))
-    stop("the vector of probabilities is missing")
+    stop("the vector of probabilities is missing\n")
   if  (missing(gn))
-    stop("the population size is missing")
+    stop("the population size is missing\n")
   data = data.frame(data)
   pn = nrow(data)
   index = 1:nrow(data)
   m = match(varname, colnames(data))
   if (any(is.na(m)))
-    stop("the name of the variable is wrong")
+    stop("the name of the variable is wrong\n")
   data2 = cbind.data.frame(data[, m], index)
   colnames(data2) = c(varname, "index")
   if (any(is.na(data[,m])))
-    stop("Missing values for some y-variables")
+    stop("Missing values for some y-variables\n")
   if (length(m) == 1) {
     htestim = crossprod(data[,m],1/pii)
-    htbc=HTcondbiasest(data=data, varname = varname, gn=gn, method = method, pii=pii)[,c(seq(1+length(data),length(data)+length(varname)))]
-    result=htestim-(min(htbc)+max(htbc))/2
+    htbc = HTcondbiasest(data=data, varname = varname, gn=gn, method = method, pii=pii)[,c(seq(1+length(data),length(data)+length(varname)))]
+    result = htestim-(min(htbc)+max(htbc))/2
   } else {
     htestim = (1/pii) %*% as.matrix(data[,m])
     htbc = HTcondbiasest(data=data, varname = varname, gn=gn, method = method, pii=pii)[,c(seq(1+length(data),length(data)+length(varname)))]
@@ -210,17 +261,17 @@
 "strata_robustest" <- function(data, strataname=NULL, varname=NULL, gnh,
                                method=c("si","poisson","rejective"), pii) {
   if (missing(gnh))
-    stop("the population size vector is missing")
+    stop("the population size vector is missing\n")
   if (missing(strataname) | is.null(strataname))
-    stop("the strata name is missing")
+    stop("no variable name to use for stratification has been specified\n")
   data = data.frame(data)
   index = 1:nrow(data)
   m = match(varname, colnames(data))
   if (any(is.na(m)))
-    stop("the name of the variable is wrong")
+    stop("the name of the variable is wrong\n")
   ms = match(strataname, colnames(data))
   if (any(is.na(ms)))
-    stop("the name of the strata is wrong")
+    stop("the name of the strata is wrong\n")
   x1 = data.frame(unique(data[,ms]))
   rht = matrix(0, nrow=nrow(x1), ncol=length(m))
 
@@ -278,30 +329,30 @@ tuningconst = function(bi, tailleseq=1000) {
   function (data, varname = NULL, gn, method="si", pii, typewin="BHR",
             tailleseq=10000, remerge=T) {
     if (missing(method)) {
-      warning("the method is not specified; by default, the method is si")
+      warning("the method is not specified; by default, the method is si\n")
       method = "si"
     }
     if (!(method %in% c("si", "poisson", "rejective")))
-      stop("the name of the method is wrong")
+      stop("the name of the method is wrong\n")
     if (missing(typewin)) {
-      warning("the type of winsorization is not specified; by default, the method is BHR")
+      warning("the type of winsorization is not specified; by default, the method is BHR\n")
       typewin = "BHR"
     }
     if (!(typewin %in% c("BHR", "standard", "DT")))
-      stop("the name of the type of winsorization  is wrong")
+      stop("the name of the type of winsorization  is wrong\n")
     if (missing(pii))
-      stop("the vector of probabilities is missing")
+      stop("the vector of probabilities is missing\n")
     data = data.frame(data)
     pn = nrow(data)
     index = 1:nrow(data)
     m = match(varname, colnames(data))
     if (any(is.na(m)))
-      stop("the name of the variable is wrong")
+      stop("the name of the variable is wrong\n")
     data2 = cbind.data.frame(data[, m], index)
     colnames(data2) = c(varname, "index")
     bc = HTcondbiasest(data, varname, gn, method, pii,remerge=F)
     if(length(m) != ncol(bc))
-      stop("the number of conditionnal bias is different from the number of variable of study")
+      stop("the number of conditionnal bias is different from the number of variable of study\n")
     if (typewin == "BHR") {
       if (length(m) == 1){
         tc = tuningconst(bc[,1],tailleseq=tailleseq)
@@ -389,23 +440,23 @@ tuningconst = function(bi, tailleseq=1000) {
                                                                     "poisson", "rejective"), pii,typewin="BHR",tailleseq=10000,remerge=T)
   {
     if  (missing(gnh))
-      stop("the population size vector is missing")
+      stop("the population size vector is missing\n")
     if (missing(strataname) | is.null(strataname))
-      stop("the strata name is missing")
+      stop("no variable name to use for stratification has been specified\n")
     data = data.frame(data)
     index = 1:nrow(data)
     m = match(varname, colnames(data))
     if (any(is.na(m)))
-      stop("the name of the variable is wrong")
+      stop("the name of the variable is wrong\n")
     ms = match(strataname, colnames(data))
     if (any(is.na(ms)))
-      stop("the name of the strata is wrong")
+      stop("the name of the strata is wrong\n")
     if (missing(typewin)) {
-      warning("the type of winsorization is not specified; by default, the method is BHR")
+      warning("the type of winsorization is not specified; by default, the method is BHR\n")
       typewin = "BHR"
     }
     if (!(typewin %in% c("BHR", "standard", "DT")))
-      stop("the name of the type of winsorization  is wrong")
+      stop("the name of the type of winsorization  is wrong\n")
     x1 = data.frame(unique(data[,ms]))
     matw=matrix(0,nrow=nrow(data),ncol=length(m))
     cgn=0
@@ -448,7 +499,7 @@ tuningconst = function(bi, tailleseq=1000) {
 
 determinconstws = function(pii, x, bi, tailleseq) {
   if (max(bi) < -min(bi)) {
-    stop("The condition for existence is not satisfied.")
+    stop("the condition for existence is not satisfied\n")
   }
   di <- 1 / pii
   functiontws = function(a, di, x, bi) {
@@ -481,7 +532,7 @@ determinconstws = function(pii, x, bi, tailleseq) {
 
 determinconstwDT = function(pii, x, bi, tailleseq=1000) {
   if (max(bi) < -min(bi)) {
-    stop("The condition for existence is not satisfied.")
+    stop("the condition for existence is not satisfied\n")
   }
   di <- 1 / pii
   functiontDT = function(a, di, x, bi){
